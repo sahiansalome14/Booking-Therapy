241cb437974aac893caa148641d4235b7d133b5b
Sahian Salome <sahiangutierrez14@gmail.com>
2026-02-21 12:03:22 -0500

    feat: add Poetry for dependency management and .env for security

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,7 +2,7 @@
 URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bookings.views import CrearReservaView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reservar/', CrearReservaView.as_view(), name='crear_reserva'),
 ]
-
